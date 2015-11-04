e7c95ff390cbf3293e9f56c10371aca7dc6b8312
louhichio <louhichio@hotmail.com>
2015-11-04 17:46:25 +0100

    git Reset

---
+++ VR_CardBoard/Assets/1_Project/2_Scripts/EventManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+public class EventManager : Singleton<EventManager> 
+{	
+	public static event Action gameReset;
+
+	public void GameReset()
+	{
+		if(gameReset != null)
+			gameReset();
+	}
+}
