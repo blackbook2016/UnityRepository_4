19b3cd4e741de5504132760f1cc5550706026cec
louhichio <louhichio@hotmail.com>
2015-11-05 16:16:26 +0100

    ProtoOG_0.1

---
+++ VR_OculusGear/Assets/1_Project/2_Scripts/ObjectDraggable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDraggable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Change();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Change()
+	{
+		GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.1f), Random.Range(0.0f, 1.1f), Random.Range(0.0f, 1.1f));
+	}
+}
